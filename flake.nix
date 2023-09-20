36921974ebad2b0a9e0bf231c4bc80890855f9a1
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2023-09-19 20:53:13 -0700

    More flake experiments.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
-  outputs = _: {
+  outputs = { self, nixpkgs }: {
     nixosModules = {
-      core = import ./core.nix;
-      laptop = import ./laptop.nix;
-      pbp = import ./pbp.nix;
-      ux = import ./ux.nix;
+      core = import ./modules/core.nix;
+      laptop = import ./modules/laptop.nix;
+      pbp = import ./modules/pbp.nix;
+      ux = import ./modules/ux.nix;
+    };
+    packages = {
+      aarch64-linux = with import nixpkgs { system = "aarch64-linux"; }; {
+        megazeux = import ./games/megazeux.nix pkgs;
+      };
     };
   };
 }
---
+++ games/megazeux.nix
@@ -0,0 +1,19 @@
+{ fetchFromGitHub, libpng, libogg, libvorbis, SDL2, stdenv, ... }:
+stdenv.mkDerivation rec {
+  pname = "megazeux";
+  version = "2.92f";
+  src = fetchFromGitHub {
+    owner = "AliceLR";
+    repo = "megazeux";
+    rev = "v${version}";
+    sha256 = "sha256-Vir3g5OLsQr49nFZwgZ31Ti37Ub30MtfAnO/0mUUnGY=";
+  };
+  buildInputs = [ libpng libogg libvorbis SDL2 ];
+  preBuild = ''
+    ./config.sh --platform unix \
+      --prefix $out/usr \
+      --sysconfdir $out/etc \
+      --gamesdir $out/bin \
+      --bindir $out/bin
+  '';
+}
