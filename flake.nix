5b1cf908ca4633bec5aae08674fb817b795b6943
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-07-26 18:11:41 -0700

    Revert "Return to Hyprland git."
    This reverts commit 8db111bae115f937accaa58a68ef27e915621c17.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url  = "github:nixos/nixpkgs/nixos-unstable";
     ags.url      = "github:aylur/ags";
-    hyprland.url = "git+https://github.com/hyprwm/hyprland?submodules=1";
+    hyprland.url = "git+https://github.com/hyprwm/hyprland?submodules=1&ref=refs/tags/v0.41.2";
     hypridle.url = "git+https://github.com/hyprwm/hypridle?submodules=1";
     hyprlock.url = "git+https://github.com/hyprwm/hyprlock?submodules=1";
   };
