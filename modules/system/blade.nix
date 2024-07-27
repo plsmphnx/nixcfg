70b8a81300483c3194ad5c1b25d87d5181f4e0ac
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-07-27 14:07:06 -0700

    Remove blade login for experimentation.

--- modules/system/blade.nix
+++ modules/system/blade.nix
@@ -3,7 +3,6 @@ inputs: { pkgs, ... }: {
     ../core.nix
     ../laptop.nix
     (import ../ux.nix inputs)
-    ../login.nix
     ../hardware/intel.nix
     ../hardware/nvidia.nix
     ../hardware/razer.nix
