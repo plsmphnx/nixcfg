6f3038287ecb37b43591bf92610743f46db44da4
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-09-07 11:54:03 -0700

    Remove custom layout in favor of bind.

--- modules/ux.nix
+++ modules/ux.nix
@@ -123,12 +123,6 @@ in {
     gvfs.enable = true;
 
     gnome.gnome-keyring.enable = true;
-
-    xserver.xkb.extraLayouts.vt = {
-      description = "US layout with only one VT switch.";
-      languages = [ "eng" ];
-      symbolsFile = ../tools/vt.xkb;
-    };
   };
 
   xdg.portal = {
