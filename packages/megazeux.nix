d17bed06e653f9145037127dbb280dd26e87c4d0
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-09-17 20:30:21 -0700

    Attempt to package steamdeck-dkms for NixOS.

--- packages/megazeux.nix
+++ packages/megazeux.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub, libpng, libogg, libvorbis, SDL2, stdenv }:
 stdenv.mkDerivation rec {
   pname = "megazeux";
-  version = "2.92f";
+  version = "2.93b";
   src = fetchFromGitHub {
     owner = "AliceLR";
     repo = "megazeux";
-    rev = "v${version}";
-    sha256 = "sha256-Vir3g5OLsQr49nFZwgZ31Ti37Ub30MtfAnO/0mUUnGY=";
+    rev = "refs/tags/v${version}";
+    sha256 = "sha256-hsKV65HUKz1Bo/Yoh3hFlfDbvZ6sWH9jMpONXCs5Y+c=";
   };
   buildInputs = [ libpng libogg libvorbis SDL2 ];
   preBuild = ''
