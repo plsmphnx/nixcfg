c7391cee9c8bebd0d0daf57cc71432e25d82a516
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2024-10-19 01:21:58 -0700

    polychromatic -> razergenie

--- modules/hardware/razer.nix
+++ modules/hardware/razer.nix
@@ -2,6 +2,7 @@
   hardware.openrazer = {
     enable = true;
     users = [ "clecompt" ];
+    batteryNotifier.enable = false;
   };
-  environment.systemPackages = with pkgs; [ polychromatic ];
+  environment.systemPackages = with pkgs; [ razergenie ];
 }
