01c14c8861f5f67c2b26da947626aeb478c2561b
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-04-11 12:04:39 -0700

    fix bad indent

--- packages/rustenv.nix
+++ packages/rustenv.nix
@@ -5,8 +5,8 @@ in runCommandLocal "rustenv" {
   buildInputs = [ libclang.lib ] ++ dev;
 } ''
   mkdir -p $out/share
-  echo "export PKG_CONFIG_PATH=$(
-    echo $(for p in ${lib.concatStringsSep " " dev}; do
+  echo "export PKG_CONFIG_PATH=$(echo $(
+    for p in ${lib.concatStringsSep " " dev}; do
       find $p -name 'pkgconfig' -type d
     done
   ) | tr ' ' ':')" >> $out/share/rustenv
