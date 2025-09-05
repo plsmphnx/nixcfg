c97daf39faea69f6f0b7e4d897fe34b6feede2e7
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2025-09-05 12:28:10 -0700

    use cachyos-gcc until llvm version stabilizes

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -7,7 +7,7 @@ inputs: { pkgs, ... }: {
   boot = {
     # github:chaotic-cx/nyx/nyxpkgs-unstable
     kernelPackages =
-      pkgs.linuxPackages_cachyos or pkgs.linuxPackages_xanmod_latest;
+      pkgs.linuxPackages_cachyos-gcc or pkgs.linuxPackages_xanmod_latest;
     kernelModules = [ "ntsync" ];
   };
 
