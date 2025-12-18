11f3254f961b56c2f324da193b8db96a29a9743a
Carl LeCompte <clecompt@microsoft.com>
2025-12-18 12:48:15 -0800

    remove substituters for now

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,17 +1,8 @@
 { outputs, pkgs, ... }: {
   imports = with outputs.nixosModules; [ library.flatpak ux ];
 
-  # cachyos.url = "github:xddxdd/nix-cachyos-kernel";
-  # { nixpkgs.overlays = [ cachyos.overlay ]; }
-  nix.settings = {
-    substituters = [ "https://attic.xuyh0120.win/lantian" ];
-    trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-  };
-
   boot = {
-    kernelPackages = if pkgs ? "cachyosKernels"
-      then pkgs.cachyosKernels.linuxPackages-cachyos-latest
-      else pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     kernelModules = [ "ntsync" ];
   };
 
