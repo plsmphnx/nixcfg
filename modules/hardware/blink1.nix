3be0de64e451f607cf37d06eed550e420d6106dc
Carl LeCompte <clecompt@microsoft.com>
2026-03-03 10:45:42 -0800

    add blink1 target

--- modules/hardware/blink1.nix
+++ modules/hardware/blink1.nix
@@ -2,4 +2,9 @@
   services.udev.extraRules = ''
     ATTRS{idVendor}=="27b8", ATTRS{idProduct}=="01ed", MODE:="666", GROUP="wheel"
   '';
+
+  systemd.user.devices.blink1 = {
+    idVendor = "27b8";
+    idProduct = "01ed";
+  };
 }
