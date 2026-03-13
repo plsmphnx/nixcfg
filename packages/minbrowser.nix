0f2c8ab710d0b866820ce66b3fa0d82c38966af0
Carl LeCompte <clecompt@microsoft.com>
2026-03-13 15:44:43 -0700

    update min

--- packages/minbrowser.nix
+++ packages/minbrowser.nix
@@ -6,7 +6,7 @@
   stdenvNoCC
 }: stdenvNoCC.mkDerivation rec {
   pname = "minbrowser";
-  version = "1.35.3";
+  version = "1.35.4";
 
   src = let
     root = "https://github.com/minbrowser/min/releases/download";
@@ -16,9 +16,9 @@
       armv7l-linux = "armv7l";
     }.${stdenvNoCC.hostPlatform.system};
     hash = {
-      amd64 = "sha256-2WR/rjJD66ue5GuE/sl1G8z6EG9YLhphJoVeepXwwH8=";
-      arm64 = "sha256-Z4oUy7k+abCwTdhPscREEc9S/gi4tYR7PlidODgPlqY=";
-      armv7l = "sha256-koUBdFPR322lfStjLwGU8yx1+1nKPvPmfGLkK+AVRi4=";
+      amd64 = "sha256-IUzAUVK0wu7jkILqiQZ9sveLHAKgujayY6LuvtAhPRc=";
+      arm64 = "sha256-VWxN1HIshgHGt2nqIGMQ1XUmSPnhmwIEWaVTi5TAzcA=";
+      armv7l = "sha256-RdQSuj1q2MnwVmx2zMQNT2liEH5bhCiru4GRVlXk/4o=";
     };
   in fetchurl {
     url = "${root}/v${version}/min-${version}-${arch}.deb";
